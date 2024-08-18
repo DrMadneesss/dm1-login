121318dafcabe9ceac55491ac782d2a69d6fe64b
DrMadneesss <joao.marcal5@fatec.sp.gov.br>
2024-08-18 20:24:18 -0300

    Estilização

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
-export default function App() {
+
+import logo from './assets/favicon.png'; 
+
+export default function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={logo}  
+        style={styles.logo}
+      />
+      <Text style={styles.loginText}>Login</Text>
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Nome de usuário"
+        autoCapitalize="none"
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+      />
+      
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Acessar</Text>
+      </TouchableOpacity>
+      
+      <Text style={styles.signupText}>
+        Não possui conta? <Text style={styles.signupLink}>faça inscrição!</Text>
+      </Text>
     </View>
   );
 }
@@ -13,8 +39,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 30,
+  },
+  loginText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 15,
+    marginBottom: 15,
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#7289da', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  signupText: {
+    fontSize: 14,
+    color: '#777',
+  },
+  signupLink: {
+    color: '#7289da', 
+    fontWeight: 'bold',
   },
 });
