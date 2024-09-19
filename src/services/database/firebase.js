cd6a5d2d464ae864b0890f430fe03d9bcf79f45c
DrMadneesss <132903700+DrMadneesss@users.noreply.github.com>
2024-09-19 07:56:42 -0300

    Update firebase.js

--- src/services/database/firebase.js
+++ src/services/database/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
  
 const firebaseConfig = {
-  apiKey: "AIzaSyD9HruOxoD_Xr557nwATzzvY0QpnEtbBGU",
-  authDomain: "app-aula-ddm1-2s24.firebaseapp.com",
-  projectId: "app-aula-ddm1-2s24",
-  storageBucket: "app-aula-ddm1-2s24.appspot.com",
-  messagingSenderId: "829251804099",
-  appId: "1:829251804099:web:7cb3d07da02bab2b933b76"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 const app = initializeApp(firebaseConfig);
  
